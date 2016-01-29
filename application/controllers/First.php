4fe2d276831ced63361b08848d0c630d45698f47
Jaggernaut555 <jay_jag6@hotmail.com>
2016-01-28 17:25:38 -0800

    Fixed tab indenting.

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -21,10 +21,10 @@ class First extends Application {
 
 	function index()
     {
-        $this->data['pagebody'] = 'justone';
-        $record = $this->quotes->first();
-        $this->data = array_merge($this->data, $record);
-
+		$this->data['pagebody'] = 'justone';
+		$record = $this->quotes->first();
+		$this->data = array_merge($this->data, $record);
+		
 		$this->render();
 	}
 
